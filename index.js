d7f3932340e94b3406d103e134367b7239f0633e
edwinolivares24@gmail.com <edwinolivares24@gmail.com>
2023-07-15 14:07:25 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 4;
+const num4 = 16;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+function calculateMod() {
+  let num = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+  while (num % 4 !== 0) {
+    num = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+  }
+  return num;
+}
+
+const mod = num3 % num4;
+
+const numbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 20];
+const max = Math.max(...numbers);
--- test/indexTest.js
+++ test/indexTest.js
@@ -11,12 +11,14 @@ describe('index.js', function() {
       expect(multiply).to.eq(62)
     });
   });
+
   describe('random', function() {
     it("generates a random integer greater than 0", function() {
       expect(Number.isInteger(random)).to.be.true
       expect(random).to.not.eq(0)
     });
   });
+
   describe('mod', function() {
     it("is an equation that calculates a remainder that is equal to 4", function() {
       expect(num3).to.be.a('number')
@@ -24,6 +26,7 @@ describe('index.js', function() {
       expect(mod).to.eq(4)
     });
   });
+  
   describe('max', function() {
     it("will return 20 as the highest number in the set", function() {
       expect(max).to.eq(20)
